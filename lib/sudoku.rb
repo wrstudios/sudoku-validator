f82abd0e6abca5fcbbb4b639b5b9b0691da48d79
Christoph Wagner <github@pandawhisperer.net>
2017-10-10 11:34:06 -0700

    Adding comments

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -5,6 +5,7 @@ class Sudoku
     @grid = grid
   end
 
+  # Parses and creates a Sudoku from a string
   def self.parse(str)
     grid = str.lines.reject do |line|
       line =~ /-/
@@ -15,14 +16,17 @@ class Sudoku
     new grid
   end
 
+  # Returns the grid row-wise (array of rows)
   def rows
     @grid
   end
 
+  # Returns the grid column-wise (array of columns)
   def cols
     @grid.transpose
   end
 
+  # Returns the grid in sub-groups (array of groups)
   def groups
     @grid.each_slice(3).flat_map do |rows|
       slices = rows.map { |row| row.each_slice(3).to_a }
--- lib/validator.rb
+++ lib/validator.rb
@@ -10,11 +10,13 @@ class Validator
   def validate
     sudoku = Sudoku.parse(@puzzle_string)
 
+    # Validate rows, columns, and sub-groups
     rows_valid = sudoku.rows.all? { |row| valid?(row) }
     cols_valid = sudoku.cols.all? { |col| valid?(col) }
     groups_valid = sudoku.groups.all? { |grp| valid?(grp) }
 
     if rows_valid && cols_valid && groups_valid
+      # A Sudoku being valid does not mean it's solved
       if sudoku.rows.any? { |row| row.count(0) > 0 }
         "This sudoku is valid, but incomplete."
       else
